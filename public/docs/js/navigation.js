e8647f8fa162b5d072d60335e1c25a1708e3dfab
Anthony Kolber <ae@aestheticallyloyal.com>
2009-10-21 15:24:08 +1100

    Created third template

--- public/docs/js/navigation.js
+++
@@ -1,9 +0,0 @@
-var nav_items = document.getElementById('navigation').getElementsByTagName('a');
-for(var i = 0; i < nav_items.length; i++) {
-	var stripped_href = nav_items[i].href.match(/([^\/]+)\/$/)[1];
-	if(document.location.href.match(stripped_href)) {
-		nav_items[i].parentNode.className = 'selected';
-		nav_items[i].parentNode.insertBefore(document.createTextNode("\u203a "), nav_items[i]);
-		break;
-	}
-}
