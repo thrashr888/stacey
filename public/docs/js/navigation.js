0ebb9c4a2881b75da7bdcb846eff1765b3acb2f1
Anthony Kolber <ae@aestheticallyloyal.com>
2009-10-29 16:08:16 +1100

    Corrected issue with loose regex within navigation highlighting javascript

--- public/docs/js/navigation.js
+++ public/docs/js/navigation.js
@@ -1,7 +1,8 @@
 var nav_items = document.getElementById('navigation').getElementsByTagName('a');
+var stripped_url = document.location.href.match(/([^\/]+)\/$/)[1];
 for(var i = 0; i < nav_items.length; i++) {
 	var stripped_href = nav_items[i].href.match(/([^\/]+)\/$/)[1];
-	if(document.location.href.match(stripped_href)) {
+	if(stripped_url == stripped_href) {
 		nav_items[i].parentNode.className = 'selected';
 		nav_items[i].parentNode.insertBefore(document.createTextNode("\u203a "), nav_items[i]);
 		break;
